5166050c47131a5da5dc6313cc31350c2e7ceb4f
hasham76 <shespirit592@gmail.com>
2025-05-03 16:00:42 +0500

    moved files to docs for git pages

---
+++ docs/index.js
@@ -0,0 +1,58 @@
+var name= "Samrah";
+//var rollNum = "1234";
+//var city = "Faisalabad";
+//var phone = "03263600592";
+//alert(name+ rollNum + "i live in"+City + "My contect Number is"+phone);
+//var nameSam = ("Samrah");
+//var city = ("Faisalabad");
+//var num =  (2+2);
+//alert(nameSam +"   I live in" + city+"    " + num);
+//for (var i=0; i<=10; i++){
+  //  document.writeln("Hello World");
+  //for(var i=0; i<=10; i++);{
+      //document.writeln("Hello World");
+  //}
+     /* var num = 2;
+      var checkNum = false;// checkNum bollein Variable ha jo ky true or false ky lie istamaal hota ha.yaa conditional hota ha jo ky if (i ===10 ) jab tak condition true nhi ho jatti loop ki shakal main chalta rhta ha
+      var dataStudent = { // yaa javascripit object ha.jis ko console.log yaan alert ki madad sy call kiya jatta ha
+      firstName: "Samrah",
+      lastName: "AKram",
+      profession: "Teacher",
+       };
+       num = parseInt(prompt("Enter a number:", num));
+       for (var i = 1; i <= 10; i++) {
+        document.write(num + " x " + i + " = " + i * num + "<br>");
+         if (i === 10) {
+           checkNum = true;
+           break;
+         }
+       }   */
+       /*var num=2;
+       var checkNum=false;
+       var Studentdata={
+        firstName : "Samrah",
+        lastName  : "Akram",
+        Profession : "lecturer",
+      };
+      num= parseInt(prompt("Enter a Number:", num));
+      for (var i=1; i<=10; i++){
+        document.write(num + "   *   "+ i  +"   =   " +  i*num  +  "<br>");
+        if (i===10){
+          checkNum=true;
+          break;
+        }
+      }*/
+     var num =2;
+     for (var i = 1; i <= 20; i++) {
+      if (i % 2 === 0) {
+        document.write(i + "<br>");
+      }
+    }
+
+      alert(Studentdata.firstName+ Studentdata.lastName  + "\n" +  Studentdata.Profession);
+       //var name = prompt("What is your name?");
+       //alert("Hello, " + name + "!");
+       
+//var num =2;
+//num =parseInt(prompt("Enter a Number:" + num));
+//var prompt = parseInt()
